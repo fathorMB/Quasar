0068e0ccadd789422e8fe09891f5f000f67421aa
fathorMB <moreno.bruschi@live.it>
2026-01-22 21:50:31 +0100

    asdad

--- src/BEAM.App.Ui.React/vite.config.ts
+++ src/BEAM.App.Ui.React/vite.config.ts
@@ -1,32 +1,42 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5174,
     proxy: {
-      '/auth': {
-        target: 'http://localhost:5288',
+      "/auth": {
+        target: "http://localhost:5288",
         changeOrigin: true,
-        secure: false
+        secure: false,
       },
-      '/api': {
-        target: 'http://localhost:5288',
+      "/api": {
+        target: "http://localhost:5288",
         changeOrigin: true,
-        secure: false
-      }
-    }
+        secure: false,
+      },
+    },
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/beam.ts'),
-      name: 'BeamUi',
-      formats: ['es'],
-      fileName: () => 'beam-ui.js'
+      entry: path.resolve(__dirname, "src/beam.ts"),
+      name: "BeamUi",
+      formats: ["umd"],
+      fileName: () => "beam-ui.js",
     },
-    outDir: path.resolve(__dirname, 'dist'),
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
-  }
+    rollupOptions: {
+      external: ["react", "react-dom", "react-router-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react-router-dom": "ReactRouterDOM",
+        },
+      },
+    },
+  },
 });
