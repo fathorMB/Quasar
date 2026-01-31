113c726283909c2c8c041af3b8690de257c94f92
Moreno BRUSCHI <moreno.bruschi@live.it>
2026-01-31 18:04:15 +0100

    dfgdfg

--- src/Quasar.Ui.React/src/main.tsx
+++ src/Quasar.Ui.React/src/main.tsx
@@ -3,6 +3,7 @@ import * as ReactDOMClient from 'react-dom/client';
 import App from './App.tsx';
 import { fetchUiConfig } from './utils/loadBundle';
 
+import * as ReactRouterDOM from 'react-router-dom';
 import * as QuasarUiReact from './index';
 
 async function start() {
@@ -10,6 +11,7 @@ async function start() {
   w.React = React;
   w.ReactDOM = ReactDOMClient;
   w.ReactDOMClient = ReactDOMClient;
+  w.ReactRouterDOM = ReactRouterDOM;
   w.QuasarUiReact = QuasarUiReact;
 
   await fetchUiConfig();
